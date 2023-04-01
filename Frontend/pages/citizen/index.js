e0eeacdc08e1777a1be58094434404c512b5ee9b
SRafi007 <sadmansakib.rafiaiub@gmail.com>
2023-04-01 14:54:59 +0600

    1st commit

---
+++ Frontend/pages/citizen/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Navbar from '../components/NavbarCustom'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>citizen Page</title>
+      </Head>
+      <Navbar/>
+      hello
+    </>
+  )
+}
